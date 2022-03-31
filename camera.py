47a5cb3268451751322b00a01c0643b6030da80c
salomaestro <chris10an.salomonsen@gmail.com>
2022-03-31 19:55:01 +0200

    back to avg position

--- camera.py
+++ camera.py
@@ -1,3 +1,4 @@
+from pkg_resources import yield_lines
 import pygame as pg
 from config import WIDTH, HEIGHT
 
@@ -7,38 +8,16 @@ class Camera:
 		self.width = width
 		self.height = height
 
-	def apply(self, entity):
-		return entity.rect.move(self.camera.topleft)
+	def apply(self, rect):
+		return rect.move(self.camera.topleft)
 
-	def update(self, *targets):
+	def update(self, targets):
+		x = -sum([t.rect.centerx for t in targets]) // len(targets) + WIDTH // 2
+		y = -sum([t.rect.centery for t in targets]) // len(targets) + HEIGHT // 2
 
-		centerx = sum([t.rect.centerx for t in targets]) / len(targets)
-		centery = sum([t.rect.centery for t in targets]) / len(targets)
+		x = min(0, x)
+		y = min(0, y)
+		x = max(-(self.width - WIDTH), x)
+		y = max(-(self.height - HEIGHT), y)
 
-		borderleft = centerx - WIDTH // 2
-		borderright = centerx + WIDTH // 2
-		bordertop = centery - HEIGHT // 2
-		borderbottom = centery + HEIGHT // 2
-
-		borderleft = min(0, borderleft)
-
-		# self.camera = pg.Rect(x, y, self.width, self.height)
-	
-	# def _center_view(self, *targets):
-		
-		# # center scrolling at the average position of all targets
-		# x = -sum([t.x for t in targets]) / len(targets)
-		# y = -sum([t.y for t in targets]) / len(targets)
-
-		# # limit scrolling to map size
-		# x = min(0, x)
-		# y = min(0, y)
-		# x = max(-(self.width - WIDTH), x)
-		# y = max(-(self.height - HEIGHT), y)
-
-		# return x, y
-	
-	# def _zoom_view(self, *targets):
-		
-		# zoom camera such that all targets are in view by adjusting width and height
-		
+		self.camera = pg.Rect(x, y, self.width, self.height)
