fd2fec612ef0fbd6714ab7d887c65b996860ed68
salomaestro <chris10an.salomonsen@gmail.com>
2022-04-02 01:07:23 +0200

    some error with typesetting

--- sprites.py
+++ sprites.py
@@ -65,7 +65,7 @@ class LaserBeam(pg.sprite.Sprite):
                 self.kill()
 
     @staticmethod
-    def rotate_img(img: pg.Surface, angle: float) -> tuple[pg.Surface, pg.Rect]:
+    def rotate_img(img, angle):
         """ Rotates image by given angle. 
         
         Args:
